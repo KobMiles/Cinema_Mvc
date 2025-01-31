d40f7f5d4384bc5f1d2e461e3f462addedac3c28
Nikita Kobylynskyi <148866440+KobMiles@users.noreply.github.com>
2025-01-31 21:35:24 +0300

    Delete _20250131_MyCinema directory

--- _20250131_MyCinema/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace _20250131_MyCinema.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
